cefba4398b745a261302cf469ad3e3fc4d001ba4
Bruno DE CARVALHO <decarvalho.bruno@free.fr>
2019-04-24 08:14:38 +0200

    -

---
+++ resources/lang/en/jsf.php
@@ -0,0 +1,32 @@
+<?php //Lang file containing translated strings for use in frontend javascript code
+
+return [
+    'login-o-auth' => [
+        'login_with_google' => 'Login with Google',
+        'login_with_twitter' => 'Login with Twitter',
+        'login_with_github' => 'Login with Github',
+    ],
+    'search' => [
+        'articles' => 'ARTICLES',
+        'authors' => 'AUTHORS',
+        'tags' => 'TAGS',
+        'no_result' => 'NO RESULTS',
+        'more_results' => 'MORE RESULTS FOR {0}',
+        'rss' => 'SEARCH RSS FEED'
+    ],
+    'wysiwyg' => [
+        'delete_link' => 'Remove link',
+        'update_link' => 'Update link',
+        'add_link' => 'Add link',
+        'no_results' => 'No results found.'
+    ],
+    'modal' => [
+        'user_delete' => [
+            'h' => 'Confirm user deletion',
+            't' => 'Do you really want to delete your account? This action is irreversible.'
+        ],
+        'user_delete_confirm' => [
+            't' => 'Your request for account deletion will be processed shortly. We will log you out now.'
+        ]
+    ]
+];
