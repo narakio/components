388b29acc5b519d34a6a44a0112695437b14535e
Bruno DE CARVALHO <decarvalho.bruno@free.fr>
2019-05-17 15:50:26 +0200

    -

---
+++ src/media/resources/lang/fr/jsb.php
@@ -0,0 +1,49 @@
+<?php
+
+return [
+    'title' => [
+        'media_edit' => '__Editing a media',
+        'media_index' => '__Managing media',
+    ],
+    'breadcrumb' => [
+        'admin-media-edit' => '__Edit Image',
+        'admin-media-index' => '__Manage media',
+    ],
+    'db' => [
+        'media' => '__Media|Media',
+        'media_alt' => '__Alt text',
+        'media_caption' => '__Caption',
+        'media_created_at' => '__Creation date',
+        'media_description' => '__Description',
+        'media_title' => '__Title',
+        'medias' => '__Media',
+    ],
+    'db_raw' => [
+        'media_title' => '__media_title',
+    ],
+    'db_raw_inv' => [
+        'media_title' => '__media_title',
+        'media_uuid' => '__media_uuid',
+    ],
+    'filters' => [
+        'media_title' => '__title',
+    ],
+    'form' => [
+        'description' => [
+            'media_title' => '__Title of the media',
+            'media_alt' => '__A text alternative to the image for screen readers or when the image does not load.',
+            'media_description' => '__For internal purposes, to help with tracking in searches.',
+            'media_caption' => '__May be displayed below the image for commentary/description purposes.',
+        ],
+    ],
+    'message' => [
+        'media_update_ok' => '__The media was updated.',
+    ],
+    'pages' => [
+        'media' => [
+            'formats_help' => '__Formats marked with <i class="fa fa-check"></i> have already been created and are ready to be served. Click on a format to create an image file with the associated dimensions.',
+            'attached_to' => '__Attached to',
+        ],
+    ],
+
+];
