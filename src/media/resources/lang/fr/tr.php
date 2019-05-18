f7216b4b429e4d8006ae0c524a712623229411b3
Bruno DE CARVALHO <decarvalho.bruno@free.fr>
2019-05-18 15:13:26 +0200

    -

---
+++ src/media/resources/lang/fr/tr.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'error' => [
+        'no_file' => '__No file to process.',
+        'type_size' => '__The file could not be processed. Make sure to check accepted file types and maximum sizes.',
+        'wrong_type' => '__The target for this media was not recognized.',
+        'entity_not_found' => '__The entity for which the media was uploaded could not be found.',
+        'entity_type' => '__The ":type" image entity type does not match anything on record.',
+        'media_type' => '__The media type ":type" does not match anything on disk.',
+    ],
+
+];
