78691b2ebf082af165aacf568b1c11ed429b2e3d
Bruno DE CARVALHO <decarvalho.bruno@free.fr>
2019-05-19 08:01:20 +0200

    -

---
+++ src/mail/resources/lang/fr/tr.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    'welcome' => [
+        'subject' => '__[:app_name] Your registration',
+        'title' => '__You registered to our website. This pleases us.',
+        'body1' => '__You can use your e-mail address and the password entered during registration to log in.',
+        'body2' => '__If you don\'t remember your password, please click on the \'forgot password\' button below the login form and set a new password.',
+        'body3' => '__Before using your account, please make sure to click on the \'activate account\' button below.',
+        'cta' => '__Activate Account',
+    ],
+    'password_reset' => [
+        'subject' => '__[:app_name] Resetting your password',
+        'title' => '__Resetting your password',
+        'body1' => '__You are receiving this e-mail because a password reset request was sent to us. Please click on the button below to change your password.',
+        'body2' => '__If you did not request a password reset, you can just delete this e-mail.',
+        'cta' => '__Reset your password',
+    ],
+    'contact' => [
+        'title' => '__A message was sent through the contact form.',
+        'email_subject' => '__[:app_name] Contact form message received',
+        'email' => '__E-mail address:',
+        'subject' => '__Subject:',
+        'message_body' => '__Message body:',
+    ],
+    'newsletter' => [
+        'title' => '__Someone subscribed to the newsletter',
+        'email_subject' => '__[:app_name] New newsletter subscription',
+        'name' => '__Name:',
+        'email' => '__Email address:',
+    ],
+    'origin' => '__This email originates from the :app_name platform and is addressed to :name (:email)',
+
+];
