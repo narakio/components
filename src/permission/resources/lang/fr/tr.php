8920a38bd2b096fd4c889371cdeaeb7df6983a81
Bruno DE CARVALHO <decarvalho.bruno@free.fr>
2019-05-20 16:27:02 +0200

    -

---
+++ src/permission/resources/lang/fr/tr.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'PERMISSION_VIEW' => '__View',
+    'PERMISSION_ADD' => '__Add',
+    'PERMISSION_EDIT' => '__Edit',
+    'PERMISSION_DELETE' => '__Delete',
+    'PERMISSION_LOGIN' => '__Login',
+    'PERMISSION_SETTINGS' => '__Settings',
+    'PERMISSION_PERMISSIONS' => '__Permissions',
+    'PERMISSION_NOTIFICATIONS' => '__Notifications',
+
+];
