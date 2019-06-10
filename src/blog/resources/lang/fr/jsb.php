4fefb4f2bde6d82cebc7b206e82c254a8f32b3e7
Bruno DE CARVALHO <decarvalho.bruno@free.fr>
2019-06-10 21:00:17 +0200

    -

---
+++ src/blog/resources/lang/fr/jsb.php
@@ -0,0 +1,84 @@
+<?php
+
+return [
+    'title' => [
+        'blog_index' => '__Listing blog posts',
+        'blog_add' => '__Managing a blog entry',
+        'blog_category' => '__Managing blog categories',
+    ],
+    'breadcrumb' => [
+        'admin-blog_posts-index' => '__Blog Posts',
+        'admin-blog_posts-add' => '__Create',
+        'admin-blog_posts-edit' => '__Edit',
+        'admin-blog_posts-category' => '__Categories',
+    ],
+    'sidebar' => [
+        'blog' => '__Blog',
+    ],
+    'db' => [
+        'blog_post_title' => '__Post title',
+        'blog_posts' => '__Blog Post|Blog Posts',
+    ],
+    'db_raw' => [
+        'blog_post_title' => '__blog_post_title',
+    ],
+    'db_raw_inv' => [
+        'blog_post_title' => '__blog_post_title',
+    ],
+    'filters' => [
+        'blog_posts_title' => '__title',
+    ],
+    'filter_labels' => [
+        'blog_posts_title' => '__Post title:',
+    ],
+    'filters_inv' => [
+        'blog_post_title' => '__title',
+    ],
+    'constants' => [
+        'BLOG_STATUS_DRAFT' => '__Draft',
+        'BLOG_STATUS_REVIEW' => '__Under review',
+        'BLOG_STATUS_PUBLISHED' => '__Published',
+    ],
+    'modal' => [
+        'blog_post_delete' => [
+            'h' => '__Confirm blog deletion',
+            't' => '__Do you really want to delete blog post "{name}"?|Do you really want to delete those {number} blog posts?',
+        ],
+    ],
+    'message' => [
+        'blog_post_delete_ok' => '__The blog post was deleted.|The {number} blog posts were deleted.',
+    ],
+    'blog' => [
+        'info_sticky' => 'This blog post is pinned. Click to unpin.',
+        'info_not_sticky' => 'Click to pin this post.',
+        'add_post' => '__Add post',
+        'add_root_button' => '__Add root category',
+        'add_source_button' => '__Add source',
+        'add_success' => '__The blog post was created.',
+        'add_tag_pholder' => '__Type enter to add tag, click to remove',
+        'author' => '__Author',
+        'blog_post_excerpt' => '__Excerpt',
+        'categories' => '__Categories',
+        'click_featured' => '__Click on an image to make it the featured image for this post.',
+        'delete_image' => '__Delete avatar',
+        'edit_image' => '__Edit image',
+        'excerpt_label' => '__This user-defined summary of the post can be displayed on the front page.',
+        'filter_name' => '__Filter by name',
+        'filter_title' => '__Filter by title',
+        'image_uploaded' => '__Upload is complete.',
+        'media' => '__Media',
+        'published_at' => '__Publishing date:',
+        'save_success' => '__The blog post was updated.',
+        'tab_available' => '__Available media',
+        'tab_upload' => '__Upload',
+    ],
+    'blog_categories' => [
+        'add_child_node' => '__Add a child element to "{name}"',
+        'edit_node' => '__Edit node "{name}"',
+        'delete_node' => '__Delete node "{name}"',
+    ],
+    'tables' => [
+        'option_del_blog' => '__Delete blog post',
+    ],
+
+];
