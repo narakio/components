798c8d6705c095a1feb3232a445aafa1103547ef
Bruno DE CARVALHO <decarvalho.bruno@free.fr>
2019-06-11 20:57:34 +0200

    -

---
+++ src/blog/resources/lang/fr/tr.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+    'most_viewed' => '__Most viewed',
+    'most_viewed_in' => '__Most viewed in :category',
+    'written_by' => '__:date by :author',
+    'page_view_count' => '__Page view count',
+    'more_reading' => '__More reading',
+    'share_facebook' => '__Share on Facebook',
+    'share_twitter' => '__Share on Twitter',
+    'categories' => '__Categories',
+    'source_types' => [
+        'url' => '__Link to resource',
+    ],
+    'category' => [
+        'entertainment' => '__Entertainment',
+        'general' => '__General',
+        'health' => '__Health',
+        'science' => '__Science',
+        'sports' => '__Sports',
+        'technology' => '__Technology',
+    ],
+    'search_headers' => [
+        'articles' => '__ARTICLES',
+        'authors' => '__AUTHORS',
+        'tags' => '__TAGS',
+        'no_result' => '__NO RESULTS',
+        'more_results' => '__MORE RESULTS FOR \':search\'',
+    ],
+    'titles' => [
+        'category' => '__Category - :name',
+        'tag' => '__Tag - :name',
+        'author' => '__Author - :name',
+    ],
+    'routes' => [
+        'blog_slug' => '__blog/{slug}',
+        'blog_cat' => '__categories/{slug}',
+        'blog_tag' => '__tag/{slug}',
+        'blog_author' => '__author/{slug}',
+    ],
+    'post_not_found' => '__We could not find a blog post by that name.',
+
+];
